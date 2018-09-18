e59955e6272a6757a8693cc8be0e7c9fd9ad0613
Romain Garcia <diabl0x61@gmail.com>
2018-09-18 14:52:08 +0200

    Completing 'isSatisfiedBy' for the 'Disjunction' class

--- src/representations/Disjunction.java
+++ src/representations/Disjunction.java
@@ -1,16 +1,35 @@
 package representations;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
- * @version 2018-09-11
+ * @version 2018-09-18
  * @author Romain Garcia
  */
 
-public class Disjunction extends Rule {
+public class Disjunction implements Constraint {
 	private Map<Variable, String> variables;
 
-	public Disjunction(Map<Variable, String> premise, Map<Variable, String> conclusion) {
-		super(premise, conclusion);
+	public Disjunction(Map<Variable, String> variables) {
+		this.variables = variables;
+	}
+
+	@Override
+	public Set<Variable> getScope() {
+		return null;
+	}
+
+	@Override
+	public boolean isSatisfiedBy(Map<Variable, String> allocation) {
+		for (Map.Entry<Variable, String> entry : variables.entrySet()) {
+			Variable key = entry.getKey();
+			String value = entry.getValue();
+
+			if (value.equals(allocation.get(key)))
+				return true;
+		}
+
+		return false;
 	}
 }
