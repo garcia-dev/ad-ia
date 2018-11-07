9791b587677835deab35f80ef61df4f1927d945a
Romain Garcia <diabl0x61@gmail.com>
2018-11-07 17:22:22 +0100

    Renaming some variables for more readability

--- src/main/java/extraction/FrequentItemsetMiner.java
+++ src/main/java/extraction/FrequentItemsetMiner.java
@@ -15,13 +15,11 @@ public class FrequentItemsetMiner {
 		Map<Set<Variable>, Double> frequentItemset = new HashMap<>();
 
 		// Adding every transaction as a frequentItemset
-		booleanDatabase.getTransactionList().forEach(map -> {
-			if (!frequentItemset.containsKey(map.keySet()))
-				frequentItemset.put(map.keySet(), 1.0);
-			else {
-				Double occurences = frequentItemset.get(map.keySet()) + 1;
-				frequentItemset.put(map.keySet(), occurences);
-			}
+		booleanDatabase.getTransactionList().forEach(transaction -> {
+			if (!frequentItemset.containsKey(transaction.keySet()))
+				frequentItemset.put(transaction.keySet(), 1.0);
+			else
+				frequentItemset.put(transaction.keySet(), frequentItemset.get(transaction.keySet()) + 1);
 		});
 
 		// Adding every variable as a frequentItemset
@@ -30,8 +28,7 @@ public class FrequentItemsetMiner {
 		// Counting the occurences of each key in frequentItemset and updating the value
 		frequentItemset.forEach((key, value) -> frequentItemset.forEach((secondKey, secondValue) -> {
 			if (secondKey != key && secondKey.containsAll(key)) {
-				Double occurences = frequentItemset.get(key) + 1;
-				frequentItemset.put(key, occurences);
+				frequentItemset.put(key, frequentItemset.get(key) + 1);
 			}
 		}));
 
@@ -39,14 +36,16 @@ public class FrequentItemsetMiner {
 		List<Set<Variable>> keyToRemoveSet = new ArrayList<>();
 
 		// Updating the value of each key to its support value, if this value is under the minimalSupport value as parameter, it's added in the list of keys to be removed
-		frequentItemset.forEach((key, value) -> {
+		for (Map.Entry<Set<Variable>, Double> entry : frequentItemset.entrySet()) {
+			Set<Variable> key = entry.getKey();
+			Double value = entry.getValue();
 			Double support = value / booleanDatabase.getTransactionList().size();
 
 			if (support >= minimalSupport)
 				frequentItemset.put(key, support);
 			else
 				keyToRemoveSet.add(key);
-		});
+		}
 
 		// Removing the keys with a lower support value than minimalSupport
 		keyToRemoveSet.forEach(frequentItemset::remove);
