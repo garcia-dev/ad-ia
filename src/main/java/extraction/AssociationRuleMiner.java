1058662c132010a3379571cc81f98c8cd1a73843
Romain Garcia <diabl0x61@gmail.com>
2018-11-07 20:40:49 +0100

    Completing the method 'calcAssociationRule'
    Not quite happy with the method used to reverse the ArrayList but a simple 'Collections.reverse(List)' worked weirdly...

--- src/main/java/extraction/AssociationRuleMiner.java
+++ src/main/java/extraction/AssociationRuleMiner.java
@@ -2,17 +2,32 @@ package extraction;
 
 import representations.Variable;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-class AssociationRuleMiner {
-	private Map<Set<Variable>, Integer> itemsetsMap;
+public class AssociationRuleMiner {
+	private Map<Set<Variable>, Double> frequentItemsets;
 
-	public AssociationRuleMiner(Map<Set<Variable>, Integer> itemsetsMap) {
-		this.itemsetsMap = itemsetsMap;
+	public AssociationRuleMiner(Map<Set<Variable>, Double> frequentItemsets) {
+		this.frequentItemsets = frequentItemsets;
 	}
 
-	public void calcAssocationRule() {
+	public Map<List<Variable>, List<Double>> calcAssociationRule() {
+		Map<List<Variable>, List<Double>> associationRuleMap = new HashMap<>();
 
+		frequentItemsets.forEach((key, value) -> {
+			if (key.size() > 1) {
+				ArrayList<Variable> keys = new ArrayList<>(key);
+
+				associationRuleMap.put(keys, new ArrayList<>(Arrays.asList(value, value / frequentItemsets.get(Set.of(keys.get(0))))));
+
+				associationRuleMap.put(IntStream.range(0, keys.size()).mapToObj(i -> keys.get(keys.size() - 1 - i)).collect(Collectors.toList()), new ArrayList<>(Arrays.asList(value, value / frequentItemsets.get(Set.of(keys.get(0))))));
+			}
+		});
+
+		associationRuleMap.forEach((key, value) -> System.out.println(key));
+
+		return associationRuleMap;
 	}
 }
