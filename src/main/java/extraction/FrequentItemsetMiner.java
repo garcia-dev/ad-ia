cadacbd438defc4df068beef6908750e668ca8dd
Romain Garcia <diabl0x61@gmail.com>
2018-11-07 16:58:27 +0100

    Completing the method 'frequentItemset'

--- src/main/java/extraction/FrequentItemsetMiner.java
+++ src/main/java/extraction/FrequentItemsetMiner.java
@@ -2,17 +2,55 @@ package extraction;
 
 import representations.Variable;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-class FrequentItemsetMiner {
+public class FrequentItemsetMiner {
 	private BooleanDatabase booleanDatabase;
 
 	public FrequentItemsetMiner(BooleanDatabase booleanDatabase) {
 		this.booleanDatabase = booleanDatabase;
 	}
 
-	public Map<Set<Variable>, Float> frequentItemsets(Float minimalSupport) {
-		return null;
+	public Map<Set<Variable>, Double> frequentItemsets(Double minimalSupport) {
+		Map<Set<Variable>, Double> frequentItemset = new HashMap<>();
+
+		// Adding every transaction as a frequentItemset
+		booleanDatabase.getTransactionList().forEach(map -> {
+			if (!frequentItemset.containsKey(map.keySet()))
+				frequentItemset.put(map.keySet(), 1.0);
+			else {
+				Double occurences = frequentItemset.get(map.keySet()) + 1;
+				frequentItemset.put(map.keySet(), occurences);
+			}
+		});
+
+		// Adding every variable as a frequentItemset
+		booleanDatabase.getVariableList().forEach(variable -> frequentItemset.put(new HashSet<>(Collections.singleton(variable)), 0.0));
+
+		// Counting the occurences of each key in frequentItemset and updating the value
+		frequentItemset.forEach((key, value) -> frequentItemset.forEach((secondKey, secondValue) -> {
+			if (secondKey != key && secondKey.containsAll(key)) {
+				Double occurences = frequentItemset.get(key) + 1;
+				frequentItemset.put(key, occurences);
+			}
+		}));
+
+		// Initializing a List of key that will be removed from frequentItemset
+		List<Set<Variable>> keyToRemoveSet = new ArrayList<>();
+
+		// Updating the value of each key to its support value, if this value is under the minimalSupport value as parameter, it's added in the list of keys to be removed
+		frequentItemset.forEach((key, value) -> {
+			Double support = value / booleanDatabase.getTransactionList().size();
+
+			if (support >= minimalSupport)
+				frequentItemset.put(key, support);
+			else
+				keyToRemoveSet.add(key);
+		});
+
+		// Removing the keys with a lower support value than minimalSupport
+		keyToRemoveSet.forEach(frequentItemset::remove);
+
+		return frequentItemset;
 	}
 }
